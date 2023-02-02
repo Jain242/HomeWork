3006677dc73452f2c4e4b968ce2bc59220d83c82
Egor <agent-nino@bk.ru>
2023-02-02 20:20:37 +0300

    HomeWork6

---
+++ HomeWork6/1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+void ReadInt (string str,int [] array) {
+int index = 0;
+int j = 0;
+string strTemp = "";
+while (str.Length != 0){
+index = str.IndexOf(" ");
+if (index == -1) {
+array[j]  = Convert.ToInt32(str);
+break;}
+strTemp = str.Substring(0, index);
+str = str.Remove(0, index+1);
+array[j]  = Convert.ToInt32(strTemp);
+j++;
+}
+}
+
+void PrintArray(int [] array){
+
+    System.Console.Write($"{String.Join(", ", array)}\n");
+}
+
+int CheckPositive (int [] array){
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{if (array[i] > 0) result +=1;}
+return result;
+}
+
+
+
+System.Console.WriteLine(" Задача 41 ");
+System.Console.WriteLine("Веедите колличество чисел: ");
+int n =  Convert.ToInt32(Console.ReadLine());
+if (n<0) {System.Console.WriteLine("Число отрцательное:");
+return;}
+int [] array = new int[n];
+System.Console.WriteLine("Веедите числа через пробел и нажмите Enter: ");
+string str1 =  System.Console.ReadLine();
+ReadInt(str1, array);
+PrintArray(array);
+System.Console.Write("Колличество чисел, которые больше нуля: ");
+System.Console.Write(CheckPositive(array));
+
+
---
+++ HomeWork6/2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void printPoint(double b1,double k1,double b2,double k2){
+double x = (b2-b1)/(k1-k2);
+double y = (k1*x) + b1;
+System.Console.WriteLine($"Точка пересечения прямых ({x};{y})");
+}
+
+
+System.Console.WriteLine(" Задача 43 ");
+System.Console.WriteLine("Введите b1 и k1:");
+double b1 = Convert.ToInt32(System.Console.ReadLine());
+double k1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите b2 и k2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{b1} ,{k1} ,{b2} ,{k2}");
+printPoint(b1,k1,b2,k2);
